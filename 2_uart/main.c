c20e8cc23c6f2a8c3c914c32684a6bf0643e3464
kvello <markus.kv1@gmail.com>
2022-04-03 11:15:56 +0200

    implement main

--- 2_uart/main.c
+++ 2_uart/main.c
@@ -3,7 +3,18 @@
 
 
 int main(){
+    gpio_init();
+    uart_init();
+    
     while(1){
+        if(((GPIO0->IN) & (1<<__BUTTON_B_PIN__)) == 0){
+            gpio_lights_on();
+            uart_send("B");
+		}
+        else if(((GPIO0->IN) & (1<<__BUTTON_A_PIN__) )== 0){
+            gpio_lights_off();
+            uart_send("A");
+		}
         
     }
     return 0;
